4a5f5b3e558de3582c68da0dca3b13add4d1a0fb
Devin Hill <33045257+iamdevinhill@users.noreply.github.com>
2023-08-02 17:23:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
-# Import the required function from the Hugging Face Transformers library
+from flask import Flask, render_template, request, jsonify
 from transformers import pipeline
 
-# Create a text generation pipeline using a specific pre-trained model
+app = Flask(__name__)
+
+# Initialize the pipeline
 pipe = pipeline("text-generation", model="meta-llama/Llama-2-7b-chat-hf", use_auth_token="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")
 
-# Define a function to interact with the chatbot
-def chat_with_bot():
-    # Print a welcome message from the bot and provide instructions
-    print("Bot: Hello! I'm your friendly chatbot. Type 'exit' to end the conversation.")
-    user_input = ""  # Initialize a variable to store user input
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-    # Start a loop that continues until the user types 'exit'
-    while user_input.lower() != "exit":
-        user_input = input("You: ")  # Prompt the user to input a message
-        # Generate a response from the chatbot based on the user's input
-        response = pipe(user_input, max_length=100, num_return_sequences=1)[0]["generated_text"]
+@app.route('/get_response', methods=['POST'])
+def get_response():
+    user_input = request.json['userInput']
+    response = pipe(user_input, max_length=100, num_return_sequences=1)[0]["generated_text"]
+    return jsonify({'response': response})
 
-        print("Bot:", response)  # Display the generated response from the bot
-
-# Entry point of the script
-if __name__ == "__main__":
-    chat_with_bot()  # Call the function to start the chatbot interaction
+if __name__ == '__main__':
+    app.run(debug=True)
 
